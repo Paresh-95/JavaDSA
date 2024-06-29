4e152cd81eb9edc6d749a145150f4c01c4966435
Paresh-95 <Pareshbhamare95@gmail.com>
2024-06-29 15:43:13 +0530

    updated this question

--- Array/WealthCustomer2d.java
+++ Array/WealthCustomer2d.java
@@ -2,6 +2,19 @@ package Array;
 
 public class WealthCustomer2d {
     public static void main(String[] args) {
-        int[][] acc = {{1, 2, 3}, {3, 2, 1}};
+       int[][] accounts = {{1,2,3},{1,2,3}};
+
+        int val  = Integer.MIN_VALUE;
+        int ans;
+        for(int i=0;i<accounts.length;i++)
+        {
+            ans = 0;
+            for(int j=0;j<accounts[0].length;j++)
+            {
+                ans = ans+accounts[i][j];
+            }
+            val  = Math.max(ans,val);
+        }
+        System.out.println(val);
     }
 }
